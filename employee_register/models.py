6a9cf23111ffd27f42ec89ba81a5a83d91ac89ce
shabbir <shabbirsafdar73@gmail.com>
2026-01-02 18:43:38 +0500

    changes in models.py

--- employee_register/models.py
+++ employee_register/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Employee(models.Model):
+class Customer(models.Model):
     name = models.CharField(max_length=15)
     email = models.EmailField()
     password = models.IntegerField()
