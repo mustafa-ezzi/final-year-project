782e16aa50da65f3351bcce27d86d32a54e7d27c
mustafa-ezzi <mustafaezzi143@gmail.com>
2026-01-05 17:44:58 +0500

    work done in admin urls

--- employee_crud/urls.py
+++ employee_crud/urls.py
@@ -20,6 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user_register.urls')),
-    path('products/', include('products.urls')),
-    
+    path('', include('products.urls')),
 ]
